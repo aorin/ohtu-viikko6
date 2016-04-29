d782d78ca55e527b3937e13168e6f4c07816542a
aorin <aorin@users.noreply.github.com>
2016-04-29 13:51:43 +0300

    kyselyitä

--- QueryLanguage1/src/main/java/statistics/Main.java
+++ QueryLanguage1/src/main/java/statistics/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
           
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
+        Matcher m = new And(new Not (new HasAtLeast(10, "goals")),
+                             new And (new HasAtLeast(10, "assists"),
+                             new PlaysIn("PHI"))
         );
         
         for (Player player : stats.matches(m)) {
